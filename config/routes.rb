7a8f121a7e63dde4d88aab3130a2a2e2c42b1f54
Kyle Yap <famasboy88@gmail.com>
2017-01-15 23:23:42 +0800

    Ready to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get '/aboutme', to: 'static_pages#aboutme'
+  get '/status', to: 'static_pages#status'
+  get '/projects', to: 'static_pages#projects'
+  get '/skillset', to: 'static_pages#skillset'
+
+ 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get aboutme" do
+    get static_pages_aboutme_url
+    assert_response :success
+    assert_select "title", "About me| Ruby on Rails Tutorial Sample App"
+  end
 
+  test "should get status" do
+    get static_pages_status_url
+    assert_response :success
+  end
+
+  test "should get skillset" do
+    get static_pages_skillset_url
+    assert_response :success
+  end
+
+  test "should get projects" do
+    get static_pages_projects_url
+    assert_response :success
+  end
+
+  ######################################
   test "should get home" do
     get root_path
     assert_response :success
@@ -25,4 +46,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
+
 end
